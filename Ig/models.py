a19c47bd83c8a4927de156d9ea6eb68ab824306e
Venus-Kavunza <venusmwende@gmail.com>
2022-04-03 17:56:06 +0300

    it will add the comments function

--- Ig/models.py
+++ Ig/models.py
@@ -72,4 +72,28 @@ class Image(models.Model):
 
     def __str__(self):
         return f'{self.user.name} Post'
+
+
+class Comments(models.Model):
+    comment = models.CharField(max_length=100)
+    post=models.ForeignKey(Image,related_name='comments',on_delete=models.CASCADE ,null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return f'{self.comment} Image'
     
+    def save_comment(self):
+        self.save()
+    
+    def delete_comment(self):
+        self.delete()
+    
+    @classmethod
+    def filter_comments_by_post_id(cls, id):
+        comments = Comments.objects.filter(post__id=id)
+        return comments
+    
+
+    class Meta:
+        ordering = ["-pk"]
