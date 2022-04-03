eae7b17567d7590865149918b055b159a480c551
Venus-Kavunza <venusmwende@gmail.com>
2022-04-03 18:24:41 +0300

    it will add the test class for profile

--- Ig/tests.py
+++ Ig/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from .models import Comments, Image, Profile
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.user = User(username='Softdev')
+        self.user.save()
+        self.user_profile = Profile(user=self.user,profile_picture="mypic.png")
+   
+    def tearDown(self):
+        User.objects.all().delete()
+        Profile.objects.all().delete()
+      
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user_profile,Profile))
+            
+    def test_save_user_profile(self):
+        self.user_profile.save_profile()
+        profiles=Profile.objects.all()
+        self.assertTrue(len(profiles)>0)
+        
+    def test_delete_user_profile(self):
+        self.user_profile.save_profile()
+        self.user_profile.delete_profile()
+        profiles=Profile.objects.all()
+        self.assertTrue(len(profiles)==0)
