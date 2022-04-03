ad4f30559adcd2600eb89dcac4419c21694866b1
Venus-Kavunza <venusmwende@gmail.com>
2022-04-03 19:28:19 +0300

    it will add the site root function

--- Ig/urls.py
+++ Ig/urls.py
@@ -1,2 +1,29 @@
 from django.conf.urls import url
 from . import views
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import pics,home,post, profile, update,add_comment,like_post,follow,unfollow,search_results,user_profile,signup_view
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth import views  as auth_views
+
+
+app_name ="instagram"
+
+
+
+path('',home,name='home'),
+path('upload/',pics, name='upload'),
+path('post/<int:pk>',post, name='post'),
+path('profile/<str:username>',profile, name='profile'),
+path('update/<int:id>',update,name='update_profile'),
+path('accounts/', include('django.contrib.auth.urls')),
+path('signup/', signup_view, name='signup'),
+path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+path('logout/', auth_views.LogoutView.as_view(next_page = '/')),
+path('follow/<int:id>',follow, name='follow'),
+path('unfollow/<int:id>',unfollow, name='unfollow'),
+path('search/',search_results, name='search_results'),
+path('user_profile/<str:username>',user_profile, name='user_profile'),
+path('add_comment/<int:post_id>',add_comment, name='add_comment'),
+path('like_post/<int:post_id>',like_post, name='like_post'),
