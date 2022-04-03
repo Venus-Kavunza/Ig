4d87741cac8607032f53399e978e4a5f71f21b8c
Venus-Kavunza <venusmwende@gmail.com>
2022-04-03 18:27:49 +0300

    it will add the test class fir the image class

--- Ig/tests.py
+++ Ig/tests.py
@@ -27,3 +27,36 @@ class ProfileTestClass(TestCase):
         self.user_profile.delete_profile()
         profiles=Profile.objects.all()
         self.assertTrue(len(profiles)==0)
+
+
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.user = User(username='Softdev')
+        self.user.save()
+        self.user_profile = Profile(user=self.user,profile_picture="mypic.png")
+        self.post = Image(name="views",caption="Nyc views",user=self.user_profile)
+        
+    def tearDown(self):
+        Image.objects.all().delete()
+        Profile.objects.all().delete()
+        User.objects.all().delete()
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post,Image))
+            
+    def test_save_post(self):
+        self.post.save_post()
+        posts=Image.objects.all()
+        self.assertTrue(len(posts)>0)
+        
+    def test_update_caption(self):
+        self.post.save_post()
+        self.post.update_post_caption(self.post.id,'test')
+        updated= Image.objects.get(caption='test')
+        self.assertEqual(updated.caption,'test') 
+        
+    def test_delete_post(self):
+        self.post.save_post()
+        self.post.delete_post()
+        posts=Image.objects.all()
+        self.assertTrue(len(posts)==0)  
