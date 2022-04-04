47fbefd90270f44f99be6d7b457b673b9fe34042
Venus-Kavunza <venusmwende@gmail.com>
2022-04-04 11:19:42 +0300

    it will add email.py

--- Ig/views.py
+++ Ig/views.py
@@ -16,3 +16,40 @@ from .email import send_welcome_email
 # Create your views here.
 
 
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            name=form.cleaned_data['fullname']
+            email=form.cleaned_data['email']
+           
+            send_welcome_email(name,email)
+
+            user = authenticate(username=username, password=password)
+
+            login(request, user)
+
+            return redirect('instagram:home')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+@login_required(login_url='/accounts/login/')
+def home_page(request):
+    current_user = request.user
+    posts = Image.objects.all()
+   
+    user = User.objects.get(username=current_user.username)
+    users = User.objects.exclude(username=current_user.username).exclude(is_superuser=True)
+  
+    ctx = {
+        'posts':posts,
+        'user':user,
+        'users':users,     
+        }
+
+    return render(request,'instagram/home.html',ctx)
+
